bce1b31877c6c09937989967d105ec07931e11d3
Alexandre C Vieira <acamargo.vieira@gmail.com>
2019-02-28 15:58:30 -0300

    tracking  trash changes

--- indicator-places.py
+++ indicator-places.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
 # Very simple app-indicator, shows gtk-bookmarks (aka places)
@@ -12,17 +12,13 @@ import gi
 gi.require_version('Gtk', '3.0')
 gi.require_version('AppIndicator3', '0.1')
 
-from gi.repository import Gtk as gtk
-from gi.repository import Gio as gio
+from gi.repository import Gtk, Gio
 from gi.repository import AppIndicator3 as AppIndicator
 
 import os
-# import gtk
-# import gio
 import signal
 import subprocess
-# import appindicator
-import urllib
+from urllib.parse import unquote
 
 APP_NAME = 'indicator-places'
 APP_VERSION = '1.0'
@@ -32,21 +28,21 @@ class IndicatorPlaces:
     LEGACY_BOOKMARKS_PATH = os.getenv('HOME') + '/.gtk-bookmarks'
     BOOKMARKS_PATH = os.getenv('HOME') + '/.config/gtk-3.0/bookmarks'
 
-    FM = gio.app_info_get_default_for_type("inode/directory", True).get_executable()
+    FM = Gio.app_info_get_default_for_type("inode/directory", True).get_executable()
 
     # print FM # for debug
 
     def __init__(self):
-        self.ind = AppIndicator.Indicator.new("places", gtk.STOCK_HOME, AppIndicator.IndicatorCategory.APPLICATION_STATUS)
+        self.ind = AppIndicator.Indicator.new("places", Gtk.STOCK_HOME, AppIndicator.IndicatorCategory.APPLICATION_STATUS)
         self.ind.set_status(AppIndicator.IndicatorStatus.ACTIVE)        
 
         self.update_menu()
 
     def create_menu_item(self, label, icon_name):
-        image = gtk.Image()
+        image = Gtk.Image()
         image.set_from_icon_name(icon_name, 24)
 
-        item = gtk.ImageMenuItem()
+        item = Gtk.ImageMenuItem()
         item.set_label(label)
         item.set_image(image)
         item.set_always_show_image(True)
@@ -57,9 +53,9 @@ class IndicatorPlaces:
         if path.startswith("smb") or path.startswith("ssh") or path.startswith("ftp") or path.startswith("network"):
             icon_name = "folder-remote"    
         else:
-            f = gio.File.new_for_uri(path)
+            f = Gio.File.new_for_uri(path)
             try:   
-                info = f.query_info(gio.FILE_ATTRIBUTE_STANDARD_ICON, 0, None)
+                info = f.query_info(Gio.FILE_ATTRIBUTE_STANDARD_ICON, 0, None)
                 icon = info.get_icon()
                 icon_name = icon.get_names()[0] if icon.get_names()[0] != '(null)' else 'folder'
             except (NameError):
@@ -67,14 +63,6 @@ class IndicatorPlaces:
         
         return icon_name
 
-    def find_trash(self):
-        home = os.getenv('HOME') + "/"
-        for dirpath, dirnames, filenames in os.walk(home):
-            for dirname in dirnames:
-                if dirname.find("Trash") == 0 or dirname.find("trash") == 0:
-                    dirname = os.path.join(dirpath, dirname)
-                    return dirname
-    
     # This methind creates a menu
     def update_menu(self, widget = None, data = None):
         try:
@@ -85,7 +73,7 @@ class IndicatorPlaces:
             bookmarks = []
 
         # Create menu
-        menu = gtk.Menu()
+        menu = Gtk.Menu()
         self.ind.set_menu(menu)
 
         # Home folder menu item
@@ -104,15 +92,22 @@ class IndicatorPlaces:
         menu.append(item)
 
         # Trash
-        trash = gio.File.new_for_path(self.find_trash())
-        trash_items = trash.query_info(gio.FILE_ATTRIBUTE_TRASH_ITEM_COUNT, gio.FILE_QUERY_INFO_NONE, None).get_attribute_uint32(gio.FILE_ATTRIBUTE_TRASH_ITEM_COUNT)
-        print trash_items # for debug
+        trash = Gio.File.new_for_uri("trash:")
+        trash_items = trash.query_info(Gio.FILE_ATTRIBUTE_TRASH_ITEM_COUNT,
+                                       Gio.FileQueryInfoFlags.NONE, None).get_attribute_uint32(
+                                           Gio.FILE_ATTRIBUTE_TRASH_ITEM_COUNT)
         item = self.create_menu_item("Trash", "user-trash")
+
+        if trash_items > 0:
+            image = Gtk.Image()
+            image.set_from_icon_name("user-trash-full", 24)
+            item.set_image(image)
+            
         item.connect("activate", self.on_bookmark_click, 'trash:')
         menu.append(item)
 
         # Show separator
-        item = gtk.SeparatorMenuItem()
+        item = Gtk.SeparatorMenuItem()
         menu.append(item)
 
         # Populate bookmarks menu items
@@ -122,47 +117,57 @@ class IndicatorPlaces:
             if not label:
                 label = os.path.basename(os.path.normpath(path))
 
-            label = urllib.unquote(label)
+            label = unquote(label)
             item = self.create_menu_item(label, self.get_bookmark_icon(path))
             item.connect("activate", self.on_bookmark_click, path)
 
             # Append the item to menu
             menu.append(item)
 
-        # Show separator
-        # item = gtk.SeparatorMenuItem()
-        # menu.append(item)
-
-        # Quit menu item
-        # item = self.create_menu_item("Quit", "gtk-quit")
-        # item.connect("activate", gtk.main_quit)
-        # menu.append(item)
-
         # Show the menu
         menu.show_all()
 
     # Open clicked bookmark
     def on_bookmark_click(self, widget, path):
-        # subprocess.Popen('/usr/bin/nautilus %s' % path, shell = True)
         subprocess.Popen(self.FM + ' %s' % path, shell = True)
-
         
     def on_bookmarks_changed(self, filemonitor, file, other_file, event_type):
-        if event_type == gio.FILE_MONITOR_EVENT_CHANGES_DONE_HINT:
-            print 'Bookmarks changed, updating menu...'
+        if event_type == Gio.FileMonitorEvent.CHANGES_DONE_HINT:
+            print ('Bookmarks changed, updating menu...')
             self.update_menu()
 
-if __name__ == "__main__":
-    # Catch CTRL-C
-    # signal.signal(signal.SIGINT, lambda signal, frame: gtk.main_quit())
+    def on_update_menu(self, filemonitor, file, other_file, event_type):
+        if event_type == Gio.FileMonitorEvent.CHANGES_DONE_HINT or Gio.FileMonitorEvent.DELETED:
+            print ('Trash changed, updating menu...')
+            self.update_menu()
 
+            
+    def find_trash(self):
+        home = os.getenv('HOME')
+        for dirpath, dirnames, filenames in os.walk(home):
+            for dirname in dirnames:
+                if dirname.find("Trash") == 0 or dirname.find("trash") == 0:
+                    dirname = os.path.join(dirpath, dirname) + "/files"
+                    return dirname
+
+if __name__ == "__main__":
     # Run the indicator
     i = IndicatorPlaces()
     
     # Monitor bookmarks changes 
-    # file = gio.File(i.BOOKMARKS_PATH)
-    # monitor = file.monitor_file()
-    # monitor.connect("changed", i.on_bookmarks_changed)            
+    if os.path.isfile(i.LEGACY_BOOKMARKS_PATH):
+        file = Gio.File.new_for_path(i.LEGACY_BOOKMARKS_PATH)
+    else:
+        file = Gio.File.new_for_path(i.BOOKMARKS_PATH)
+           
+    monitor = file.monitor_file(Gio.FileMonitorFlags.NONE, None)
+    monitor.connect("changed", i.on_bookmarks_changed)
+
+    # Monitor Trash changes
+    trash_path = i.find_trash()
+    trash = Gio.File.new_for_path(trash_path)
+    t_monitor = trash.monitor_directory(Gio.FileMonitorFlags.NONE, None)
+    t_monitor.connect("changed", i.on_update_menu)
     
     # Main gtk loop
-    gtk.main()
+    Gtk.main()
