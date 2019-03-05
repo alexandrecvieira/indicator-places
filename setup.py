6f83b514b3e4bc3a48f75c313288203eb0dc6692
Alexandre C Vieira <acamargo.vieira@gmail.com>
2019-03-04 23:42:20 -0300

    setup.py e tradução

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+import glob
+import os
+from DistUtilsExtra.command import *
+
+
+
+
+setup(name='indicator-places',
+      version='1.0.0',
+      description='Simple indicator places for Stalonetray/Openbox',
+      author='Alexandre C Vieira',
+      author_email='acamargo.vieira@gmail.com',
+      url='https://github.com/alexandrecvieira/indicator-places',
+      scripts=[
+          'indicator-places',
+      ],
+      cmdclass={"build": build_extra.build_extra,
+                "build_i18n": build_i18n.build_i18n}
+      )
